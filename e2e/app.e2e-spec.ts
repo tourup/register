94f8b514ca6d1d99ed867a123ac006fa54b15e08
af4ro <1997anshul@gmail.com>
2017-07-16 15:54:59 -0700

    Updating config:

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TourupLoginPage } from './app.po';
+
+describe('tourup-login App', () => {
+  let page: TourupLoginPage;
+
+  beforeEach(() => {
+    page = new TourupLoginPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
